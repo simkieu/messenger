ed5f68474d43d8bcccdbe02f80dcbc1a2f7ec48e
Sim Kieu <simkieu@gmail.com>
2021-04-28 22:45:47 -0700

    Update UI when send a message, server request has not been sent yet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  function onMessageChange(e) {
+    setMessage(e.target.value);
+  }
+
+  function handleClick() {
+    setMessages(messages => [...messages, message]);
+    // TODO: Send a request to server
+    setMessage('');
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      handleClick();
+    }
+  }
+
   return (
     <div className="App">
-      Hi
+      <input value={message} onChange={onMessageChange} onKeyDown={handleKeyDown}></input>
+      <button className="SendButton" onClick={handleClick}>Send</button>
+      {messages.map(message => <div>{message}</div>)}
     </div>
   );
 }
